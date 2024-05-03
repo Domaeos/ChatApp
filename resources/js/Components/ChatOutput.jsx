5fa716344bda0c771a68eda3915ef3a0468a106d
Domaeos <ryanmurphy.ic@icloud.com>
2024-05-03 12:57:25 +0100

    Friend migration and model

--- resources/js/Components/ChatOutput.jsx
+++ resources/js/Components/ChatOutput.jsx
@@ -34,28 +34,30 @@ export default function ChatOutput({ currentRoom, roomNumberToRefresh }) {
     }, [currentRoom, roomNumberToRefresh]);
 
     return (
-        <div className="chatbox-flex">
-            {messages?.map((message, index) => {
-                return (
-                    <div
-                        className="single-chat-message"
-                        key={message?.user.id + index}
-                    >
-                        <div className="single-chat-header">
-                            <div className="single-chat-author">
-                                {message?.user.name}
+        <div className="chatbox-wrapper">
+            <div className="chatbox-flex">
+                {messages?.map((message, index) => {
+                    return (
+                        <div
+                            className="single-chat-message"
+                            key={message?.user.id + index}
+                        >
+                            <div className="single-chat-header">
+                                <div className="single-chat-author">
+                                    {message?.user.name}
+                                </div>
+                                <span className="chat-message-date">
+                                    {dayjs(message?.created_at).fromNow()}
+                                </span>
                             </div>
-                            <span className="chat-message-date">
-                                {dayjs(message?.created_at).fromNow()}
-                            </span>
+                            <div className="chat-output-message">
+                                {message.message}
+                            </div>
                         </div>
-                        <div className="chat-output-message">
-                            {message.message}
-                        </div>
-                    </div>
-                );
-            })}
-            {!currentRoom && <h1>You need to join a room</h1>}
+                    );
+                })}
+                {!currentRoom && <h1>You need to join a room</h1>}
+            </div>
         </div>
     );
 }
